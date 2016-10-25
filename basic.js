26960bf502e98ef9f943e3972c95c5b0d5431ca9
Jeff Yoon <jeyoon@outlook.com>
2016-10-25 18:25:55 +0900

    first commit

---
+++ basic.js
@@ -0,0 +1,35 @@
+// One lien Comment
+
+/*
+   This is a comment
+   More line comment
+ */
+
+// strict mode for safe coding in javascript (ES6)
+"use strict";
+
+var i = 0
+let j = 0
+const c = 0
+
+console.log(i)
+console.log(j)
+console.log(c)
+
+var a;
+console.log("a value " + a)
+// console.log("b value " + b)     // ReferenceError 예외 던짐
+
+
+// block
+if (true) {
+  var x = 5;
+}
+console.log(x); // 5
+
+if (true) {
+    let y = 6;
+    console.log(y)
+}
+console.log(y)  // ReferenceError: y is not defined
+
